ae3f94d155491afb1b56ed61d7260e9d24d567d5
Kojo-Jr <omarieben@outlook.com>
2024-06-06 09:40:47 +0000

    update

---
+++ mockData/category.data.js
@@ -0,0 +1,62 @@
+export const categoriesData = [
+  {
+    id: 0,
+    title: "Electronics",
+    image: require("../assets/mock_images/music _icon.png")
+  },
+  {
+    id: 1,
+    title: "Electronics",
+    image: require("../assets/mock_images/Electronics.png")
+  },
+  {
+    id: 2,
+    title: "Fashion",
+    image: require("../assets/mock_images/Fashion_icon.png")
+  },
+  {
+    id: 3,
+    title: "Web Design",
+    image: require("../assets/mock_images/web_design_icon.png")
+  },
+  {
+    id: 4,
+    title: "Digital Service",
+    image: require("../assets/mock_images/digital_service.png")
+  },
+  {
+    id: 5,
+    title: "Agriculture",
+    image: require("../assets/mock_images/agriculture_icon.png")
+  },
+  {
+    id: 6,
+    title: "Agriculture",
+    image: require("../assets/mock_images/agriculture_icon.png")
+  },
+  {
+    id: 7,
+    title: "Agriculture",
+    image: require("../assets/mock_images/agriculture_icon.png")
+  },
+  {
+    id: 8,
+    title: "Agriculture",
+    image: require("../assets/mock_images/agriculture_icon.png")
+  },
+  {
+    id: 9,
+    title: "Agriculture",
+    image: require("../assets/mock_images/agriculture_icon.png")
+  },
+  {
+    id: 10,
+    title: "Agriculture",
+    image: require("../assets/mock_images/agriculture_icon.png")
+  },
+  {
+    id: 11,
+    title: "Agriculture",
+    image: require("../assets/mock_images/agriculture_icon.png")
+  }
+];
