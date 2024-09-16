4052ffd499983a11ce97e36626955daed001ecfc
kojo-jr <omarieben@outlook.com>
2024-09-16 08:56:54 +0000

    updates

--- src/screens/Home/AllProducts/ShowAllProducts.jsx
+++ src/screens/Home/AllProducts/ShowAllProducts.jsx
@@ -1,11 +1,38 @@
 import { View, Text, ImageBackground, StyleSheet } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-
+import { NavigationHeader } from "../../../components/Headers";
+import { useNavigation } from "@react-navigation/native";
+import { widthPercentageToDP as wp } from "react-native-responsive-screen";
+import { FlashList } from "@shopify/flash-list";
+import { ProductCard } from "../../../components/Cards";
+import { productsData } from "../../../../mockData/products.data";
 const ShowAllProducts = () => {
+  const navigation = useNavigation();
+  const homeScreen = () => {
+    navigation.navigate("HomeScreen");
+  };
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+      <NavigationHeader headerTitle="Products" handleNavigation={homeScreen} />
+      <View style={styles.flashlistContainer}>
+        <FlashList
+          data={productsData}
+          renderItem={({ item }) => {
+            return (
+              <ProductCard
+                date={item.date}
+                featuredImage={item.featuredImage}
+                name={item.name}
+                location={item.location}
+              />
+            );
+          }}
+          estimatedItemSize={80}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
     </View>
   );
 };
@@ -16,5 +43,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fafafa"
+  },
+  flashlistContainer: {
+    marginTop: wp(20),
+    borderWidth: 1,
+    borderColor: "red",
+    flexGrow: 1
   }
 });
